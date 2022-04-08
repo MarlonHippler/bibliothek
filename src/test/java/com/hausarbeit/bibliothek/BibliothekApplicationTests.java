4658807be501d028ed4090dcfa2ba2d42922a085
Marlon Hippler <marlon.hippler@nordakademie.de>
2022-04-08 14:07:04 +0200

    Initial commit

---
+++ src/test/java/com/hausarbeit/bibliothek/BibliothekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hausarbeit.bibliothek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliothekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
