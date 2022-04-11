303c1b0ff0d91154f00cf878442d2cd661ff9ed4
Marc Wortmann <marc.wortmann@nordakademie.de>
2022-04-11 10:59:29 +0200

    set-up angular und generieren der ersten komponenten angelehnt an das domain model

--- client/angular-social/src/app/app.module.ts
+++ client/angular-social/src/app/app.module.ts
@@ -4,23 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
-import { Pub } from './pub.uebersicht/pub.uebersicht.component';
-import { Pub } from './pub.anlegen/pub.anlegen.component';
-import { Pub } from './pub.ausleihen/pub.ausleihen.component';
-import { Leihvorgaenge } from './leihvorgaenge.uebersicht/leihvorgaenge.uebersicht.component';
-import { Leihvorgang } from './leihvorgang.verlaengern/leihvorgang.verlaengern.component';
-import { Pub } from './pub.rueckgabe/pub.rueckgabe.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    Pub.UebersichtComponent,
-    Pub.AnlegenComponent,
-    Pub.AusleihenComponent,
-    Leihvorgaenge.UebersichtComponent,
-    Leihvorgang.VerlaengernComponent,
-    Pub.RueckgabeComponent
+
+
+
   ],
   imports: [
     BrowserModule,
