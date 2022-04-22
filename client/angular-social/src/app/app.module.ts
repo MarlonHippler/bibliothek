d0fa8f79a7ecb55f5c86a99c9bc9ca394f176f78
Marc Wortmann <marc.wortmann@nordakademie.de>
2022-04-22 11:29:19 +0200

    routing fertig aufgesetzt

--- client/angular-social/src/app/app.module.ts
+++ client/angular-social/src/app/app.module.ts
@@ -1,25 +1,26 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 import {LeihvorgangVerlaengernComponent} from './leihvorgang-verlaengern/leihvorgang-verlaengern.component' ;
-import { AppRoutingALTModule } from './app-routingALT.module';
-import { AppComponent } from './app.component';
-import { MenuComponent } from './menu/menu.component';
-import { LeihvorgaengeUebersichtComponent } from './leihvorgaenge-uebersicht/leihvorgaenge-uebersicht.component' ;
+import {AppRoutingALTModule} from './app-routingALT.module';
+import {AppComponent} from './app.component';
+import {MenuComponent} from './menu/menu.component';
+import {LeihvorgaengeUebersichtComponent} from './leihvorgaenge-uebersicht/leihvorgaenge-uebersicht.component' ;
 import {PubAusleihenComponent} from "./pub-ausleihen/pub-ausleihen.component";
 import {PubRueckgabeComponent} from "./pub-rueckgabe/pub-rueckgabe.component";
 import {PubUebersichtComponent} from "./pub-uebersicht/pub-uebersicht.component";
-import { PubAnlegenComponent } from './pub-anlegen/pub-anlegen.component';
-import {NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {PubAnlegenComponent} from './pub-anlegen/pub-anlegen.component';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 import {MatInputModule} from "@angular/material/input";
 import {ReactiveFormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
+import {Routes} from "@angular/router";
+import {CommonModule} from "@angular/common";
 import {RouterModule} from "@angular/router";
-import { AppRoutingModule } from './app-routing.module';
 
 
 @NgModule({
   declarations: [
-
+    AppComponent,
     MenuComponent,
     LeihvorgangVerlaengernComponent,
     LeihvorgaengeUebersichtComponent,
@@ -33,15 +34,19 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     AppRoutingALTModule,
-    RouterModule.forRoot(RouterModule),
     NoopAnimationsModule,
+    RouterModule.forRoot([{path: 'menu', component: MenuComponent},
+      {path: 'anlegen', component: PubAnlegenComponent},
+      {path: 'ausleihen', component: PubAusleihenComponent}]),
     MatInputModule,
     ReactiveFormsModule,
     HttpClientModule,
-    AppRoutingModule,
+    CommonModule,
+
 
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
