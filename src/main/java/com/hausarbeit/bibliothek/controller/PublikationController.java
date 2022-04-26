a5b7a44700e4cfaac1c263b81897603fc933c5ee
Marlon Hippler <marlon.hippler@nordakademie.de>
2022-04-26 15:07:20 +0200

    Publikationlöschen Adresse geändert

--- src/main/java/com/hausarbeit/bibliothek/controller/PublikationController.java
+++ src/main/java/com/hausarbeit/bibliothek/controller/PublikationController.java
@@ -35,7 +35,7 @@ public class PublikationController {
     }
 
 
-    @DeleteMapping(path = "{publikationID}")
+    @DeleteMapping(path = "publikation/loeschen/{publikationID}")
     public void publikationLoeschen(@PathVariable("publikationID") Long publikationID){
         publikationservice.publikationLoeschen(publikationID);
     }
